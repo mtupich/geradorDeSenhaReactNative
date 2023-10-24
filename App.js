9fd86e24241d5ce04d916e3adcbab56252b74ec1
Maria <mariatupich@gmail.com>
2023-10-24 16:00:52 -0300

    Estilização da modal e exibição da senha gerada

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { useState } from 'react'
-import { View, Text, StyleSheet, Image, Button, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, Image, Button, TouchableOpacity, Modal } from 'react-native'
 import Slider from '@react-native-community/slider'
+import { ModalPassword } from './src/assets/components/modal'
 
 export default function App() {
-  const [size, setSize] = useState(8)
   let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+  // padrão pra utilização de hook (use state), retorna um array de duas posições, a primeira é valor atual do estado e a segunda posição contém uma func que permite modificar o estado
+  const [size, setSize] = useState(8)
+  const [passwordValue, setPasswordValue] = useState("")
+  const [modalVisible, setModalVisible] = useState(false)
 
   function generatePassword() {
     let password = ""
     for(let item = 0, n = charset.length; item < size; item++){
       password += charset.charAt(Math.floor(Math.random() * n))
     }
-
-    console.log(password)
+    setPasswordValue(password)
+    setModalVisible(true);
   }
-
+  console.log(`passei aqui 1: ${passwordValue}`);
   return (
     <View style={styles.container}>
       
@@ -32,7 +37,6 @@ export default function App() {
           style={{ height: 50 }}
           minimumValue={6}
           maximumValue={20}
-          maximumTrackTintColor="#ff0000"
           maximumTrackTintColor="rgba(255, 0, 0, 0.2)" 
           minimumTrackTintColor="#000"
           thumbTintColor="#392de9"
@@ -45,9 +49,14 @@ export default function App() {
         <Text style={styles.buttonText}>Gerar senha</Text>
       </TouchableOpacity>
     
+      <Modal visible={modalVisible} animationType="fade" transparent={true}>
+          <ModalPassword password={passwordValue} handleClose={ () => setModalVisible(false) }/>
+      </Modal>
+
+
     </View>
   )
-}
+}                                         
 
 const styles = StyleSheet.create({
   container:{
